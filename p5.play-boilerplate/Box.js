b012e475c45d0a3477cad8e98557e672d91d4994
Mithilessh2010 <71537184+Mithilessh2010@users.noreply.github.com>
2020-11-15 15:29:36 -0800

    Add files via upload

---
+++ p5.play-boilerplate/Box.js
@@ -0,0 +1,39 @@
+class Box {
+  constructor(x, y, width, height, color) {
+    var options = {
+      'restitution': 0.7,
+      'friction': 0.6,
+      'isStatic': false
+    }
+    this.visibility = 255;
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.speed = this.body.speed;
+    World.add(world, this.body);
+  }
+  display() {
+    var pos = this.body.position;
+    var angle = this.angle;
+    console.log(this.body.speed);
+    push();
+
+    if (this.speed < 3) {
+      translate(this.body.position.x, this.body.position.y);
+      fill(this.color);
+      rotate(angle);
+      rectMode(CENTER);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+    else {
+      push();
+      World.remove(world, this.body);
+      this.visibility = this.visibility - 5;
+      tint(255, this.visibility);
+      pop();
+    }
+
+  }
+}
